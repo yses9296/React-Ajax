247c4f8ec4e66f24802ee647a90fd142c0762411
Eunseo Choi <yses9296@gmail.com>
2022-07-11 21:12:18 +0900

    2022-07-11

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">WEB</h1>
+      <nav className="menu">
+        <ul>
+          <li><a href="1">HTML</a></li>
+          <li><a href="2">CSS</a></li>
+          <li><a href="3">JS</a></li>
+        </ul>
+      </nav>
+      <article className="article">
+        <h2>Welcome, React &amp; Ajax</h2>
+      </article>
+    </div>
+  );
+}
+
+export default App;
